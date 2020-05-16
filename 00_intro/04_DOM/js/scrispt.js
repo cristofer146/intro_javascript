53f162b2e7b02ab3075e863c9b5af3543f784fb0
cristofer <cristofesanchez@gmail.com>
2020-05-16 02:57:41 +0200

    Trabajo final

---
+++ 00_intro/04_DOM/js/scrispt.js
@@ -0,0 +1,34 @@
+/*let titulo1 = document.querySelector("h1");
+
+
+titulo1.id = "titulo_principal";
+
+titulo1.classList.add("resaltado");
+
+titulo1.classList.add("color_red");
+
+titulo1.innerHTML = "Datos desde js";
+
+titulo1.innerHTML = "hola <i>a todos! </i>";
+
+console.log(titulo1);*/
+
+/*console.log(document );
+
+console.log(document.URL );
+
+console.log(document.body );
+
+console.log(document.head );
+
+console.log(document.links );
+
+console.log(document.title );
+
+document.title = "titulo cambiado desde js";
+
+document.body.value = "";
+
+document.addEventListener("click", function(){
+ alert("Hola, haz hecho click en el documento");
+});*/
---
+++ 00_intro/githubgram/js/script.js
@@ -0,0 +1,61 @@
+const imgPerfil = document.querySelector(".imagen_perfil");
+
+const tituloNombre = document.querySelector("h2.nombre");
+
+const parrafoNombreUsuario = document.querySelector(".nombre_usuario");
+
+const spanSeguidores = document.querySelector(".seguidores");
+
+const spanSiguiendo = document.querySelector(".siguiendo");
+
+const ParrafoBio = document.querySelector(".bio");
+
+const btnBuscar = document.querySelector("#buscar");
+
+const barraBusqueda = document.querySelector("#barra_busqueda");
+
+
+
+btnBuscar.addEventListener("click", function () {
+  obtenerDatosGitHub();
+});
+
+/*
+imgPerfil.src = "https://avatars3.githubusercontent.com/u/59875474?v=4"
+
+tituloNombre.textContent = user.name;
+
+parrafoNombreUsuario.textContent = user.login;
+
+spanSeguidores.textContent = user.followers;
+
+spanSiguiendo.textContent = user.following;
+
+*/
+
+async function obtenerDatosGitHub() {
+  //console.log("vamos a obtener los datos");
+
+  let UsuarioBusqueda = barraBusqueda.value;
+
+  let respuesta = await fetch("https://api.github.com/users/"+UsuarioBusqueda);
+
+  if (respuesta.ok) {
+
+    let inforUsuario = await respuesta.json();
+
+    imgPerfil.src = "https://avatars3.githubusercontent.com/u/59875474?v=4";
+
+    tituloNombre.textContent = inforUsuario.name;
+
+    parrafoNombreUsuario.textContent = inforUsuario.login;
+
+    spanSeguidores.textContent = inforUsuario.followers;
+
+    spanSiguiendo.textContent = inforUsuario.following;
+
+    ParrafoBio.textContent = inforUsuario.bio;
+  } else {
+    console.log("Ocurrio un problema");
+  }
+}
