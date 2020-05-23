c7908690663e6f18febd8f99e83baf5a876ec3cd
cristofer <cristofesanchez@gmail.com>
2020-05-23 16:24:20 +0200

    Ejemplo de la tarea

---
+++ 00_intro/ejemplo_constante.js
@@ -0,0 +1,18 @@
+/*const a = {
+  name: "cristofer",
+};
+
+console.log(a.name);*/
+
+const a = 12; {
+  console.log(a);
+
+  const b = 14;
+
+  if(true) {
+      console.log(b);
+      
+  }
+
+}
+
