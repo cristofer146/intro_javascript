ad231fc107013a5cd1967401d7235ec49845970d
cristofer <cristofesanchez@gmail.com>
2020-05-23 22:59:59 +0200

    Ejemplo_de_tarea

---
+++ 00_intro/ejemplo_de_la_tarea/tipos_de_variables.js
@@ -0,0 +1,39 @@
+//variable de teXto
+let textoA = "hola mundo"
+
+
+console.log(textoA);
+
+
+//variables tipo numero
+var x, y;
+
+x = 10;
+y = 3.7;
+
+console.log(x + y);
+
+console.log(x - y);
+
+//Variables de tipo booleano
+
+let s, t;
+
+s = true;
+y = false;
+
+console.log(s && y);
+
+//Variables tipo listo
+var frutas = ["aguacate","pera","manzana"];
+
+console.log(frutas[0]);
+
+//variables tipo objetos
+let persona = {
+    numero: "cristofer",
+    apellido: "sanchez",
+    edad: "20"
+}
+
+console.log(persona);
