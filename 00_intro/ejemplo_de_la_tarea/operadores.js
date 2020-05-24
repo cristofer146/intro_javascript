ba22e6a0e4e1546e9373c36af84f7c38b886290d
cristofer <cristofesanchez@gmail.com>
2020-05-24 04:37:52 +0200

    Ejemplo de la tarea

---
+++ 00_intro/ejemplo_de_la_tarea/operadores.js
@@ -0,0 +1,12 @@
+//operadores matematicos
+//var prioridad = (3 + 5) * 23;
+
+//alert (prioridad);
+
+//operadores string
+var nombre = "Angel";
+var apellido = "perez";
+
+var nombreCompleto = nombre + " " + apellido;
+
+alert(nombreCompleto);
